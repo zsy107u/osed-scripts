dca0a917bcaffec88699cf75bfcafa16b41334f5
zsy107u <147444983+zsy107u@users.noreply.github.com>
2026-02-08 21:15:16 +0800

    Clean up temporary files in install-mona.sh
    Remove temporary files from /tmp before installation.

--- install-mona.sh
+++ install-mona.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf /tmp/tmp.*
+
 TOOLS=("https://github.com/corelan/windbglib/raw/master/pykd/pykd.zip" "https://github.com/corelan/windbglib/raw/master/windbglib.py" "https://github.com/corelan/mona/raw/master/mona.py" "https://www.python.org/ftp/python/2.7.17/python-2.7.17.msi" "https://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe" "https://raw.githubusercontent.com/zsy107u/osed-scripts/main/install-mona.ps1")
 
 TMPDIR=$(mktemp -d)
